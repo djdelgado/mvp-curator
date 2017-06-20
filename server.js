caa24ff224a2aa0d06143f630cb690f18bcb52c9
David Delgado <djd9062@gmail.com>
2017-06-19 20:17:12 -0500

    login works

--- server.js
+++ server.js
@@ -12,10 +12,21 @@ mongoose.connect('mongodb://djdelgado:abcd1234@ds131782.mlab.com:31782/estylo', 
     else {console.log('db connected')}
     
 })
+let Users = mongoose.Schema({
+    username: String,
+    password: String
+});
+let Artist = mongoose.Schema({
+    name: String,
+    likes: Number,
+})
 
+let user = mongoose.model('user', Users);
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(PORT, function() {
-            console.log(`Listening at ${PORT}`);
+    console.log(`Listening at ${PORT}`);
 });
 
 app.use(bodyParser.urlencoded({extended: true}))
@@ -25,21 +36,32 @@ app.get('/', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-
+    console.log(req.body)
+    user.find(req.body, (err, login) => {
+        if(err){console.log(err)}
+        console.log(login)
+        if(!login[0]){
+          new user(req.body).save(err => {
+            if(err){console.log(err)}
+            console.log('Posted!')
+          });
+        }
+    });
+    
 })
 
 let options = {
     method: 'GET',
-    url: 'https://api.artsy.net/api/artist/andy-warhol',
+    url: 'https://api.artsy.net/api/artists/andy-warhol',
     headers: {
         'X-Xapp-Token': process.env.ARTSY_TOKEN,
     }
 }
 
-request(options)
-    .then((body) => {
-        console.log(body, "infoooooooo")
-    })
-    .catch(err => {
-        console.log(err)
-    })
+// request(options)
+//     .then((body) => {
+//         console.log(JSON.parse(body), "infoooooooo")
+//     })
+//     .catch(err => {
+//         console.log(err)
+//     })
